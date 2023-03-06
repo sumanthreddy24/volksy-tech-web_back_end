200a46e56b3c62846527551b213786f1ffd0da00
sumanthreddy24 <sumantht@brightcomgroup.com>
2023-03-06 04:11:04 +0000

    new file

--- 101-iterateThroughObject.js
+++
@@ -1,8 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-  for (const r of reportWithIterator) {
-    employees.push(r);
-  }
-
-  return employees.join(' | ');
-}
